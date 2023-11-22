15009d2fcc0a392d7b3f73249f1af9f910b2a448
MrRuho <paajanen.ville@gmail.com>
2023-11-22 23:13:45 +0200

    ci testiä 1.1

--- src/tests/robot/AppLibrary.py
+++ src/tests/robot/AppLibrary.py
@@ -1,8 +1,21 @@
 import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 class AppLibrary:
-    def __init__(self):
+    def __init__(self, use_headless=True):
         self._base_url = "http://localhost:5000"
+        
+        # Luodaan ChromeOptions-olio
+        chrome_options = Options()
+
+        if use_headless:
+            # Lisätään headless-tila
+            chrome_options.add_argument('--headless')
+            chrome_options.add_argument('--disable-gpu')
+
+        # Luodaan WebDriver-olio ChromeOptions-oliolla
+        self.driver = webdriver.Chrome(options=chrome_options)
 
         self.reset_application()
 
